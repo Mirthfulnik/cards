f395dc59aa02e90f90c702195ff5fa485d635a09
Mirthfulnik <nikita-sinopalnikov@yandex.ru>
2026-04-16 01:28:41 +0300

    Update releases.js

--- releases.js
+++ releases.js
@@ -6,8 +6,8 @@ const RELEASES = [
     id: 'sayan_malboro',
     artist: 'Sayan',
     title: 'Мальборо',
-    cover: 'https://dl.dropboxusercontent.com/scl/fi/d2lkhhxas7lsnuquqeho1/3000.jpg?rlkey=1onrc0rf0y4k2nqneeqje5tx1&st=9pyh8pjk',
-    video: 'https://dl.dropboxusercontent.com/scl/fi/jdpwyqto6uyv90bnh6q2u/1004-SAYAN-get.gt.mp4?rlkey=qbr4h4cj64jqkeowffojeo7za&st=l5t6cyo5',
+    cover: 'assets/releases/sayan/cover.jpg',
+    video: 'assets/releases/sayan/video.mp4',
     links: {
       yandex:  'https://music.yandex.ru/album/41489388/track/150053393?utm_source=web&utm_medium=copy_link',
       vk:      'https://vk.com/audio-2001621469_148621469',
